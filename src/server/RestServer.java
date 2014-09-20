3bb3a697fec1196e19e51bb781572690eacea126
LMO <lam@cphbusiness.dk>
2014-09-20 13:34:29 +0200

    Server now has a "default" index.html page

--- src/server/RestServer.java
+++ src/server/RestServer.java
@@ -25,6 +25,8 @@ public class RestServer {
   static int port = 8080;
   static String ip = "127.0.0.1";
   static String publicFolder = "src/htmlFiles/";
+  static String startFile = "index.html";
+  static String filesUri = "/pages";
 
   public void run() throws IOException {
     HttpServer server = HttpServer.create(new InetSocketAddress(ip, port), 0);
@@ -32,7 +34,16 @@ public class RestServer {
     server.createContext("/Date", new HandlerDate());
     server.createContext("/AllPlayerNames", new HandlerQuestion1());
     //HTTP Server Routes
-    server.createContext("/pages", new HandlerFileServer());
+    server.createContext(filesUri, new HandlerFileServer());
+    server.createContext("/", new HttpHandler() {
+      @Override
+      public void handle(HttpExchange he) throws IOException {
+        String redirect = "http://localhost:8080/pages/index.html";
+        System.out.println(redirect);
+        he.getResponseHeaders().add("Location", redirect);
+        he.sendResponseHeaders(301, 0);
+      }
+    });
     server.start();
     System.out.println("Server started, listening on port: " + port);
   }
@@ -51,9 +62,8 @@ public class RestServer {
     @Override
     public void handle(HttpExchange he) throws IOException {
       String response = new Date().toString();
-      
+
       he.getResponseHeaders().add("Access-Control-Allow-Origin", "*");
-      //he.getResponseHeaders().add("Content-Type", "appliaction/json");
       he.getResponseHeaders().add("Content-Type", "text/plain");
       he.sendResponseHeaders(200, response.length());
       try (PrintWriter pw = new PrintWriter(he.getResponseBody())) {
@@ -66,14 +76,14 @@ public class RestServer {
 
     @Override
     public void handle(HttpExchange he) throws IOException {
-      
-     String response = new Gson().toJson(getPlayers());
-     System.out.println(response);
+
+      String response = new Gson().toJson(getPlayers());
+      System.out.println(response);
       //he.getResponseHeaders().add("Access-Control-Allow-Origin", "*");
       he.getResponseHeaders().add("Content-Type", "application/json");
       he.sendResponseHeaders(200, 0);
-      try (PrintWriter pw = new PrintWriter(he.getResponseBody())) {
-        pw.print(response); //What happens if we use a println instead of print --> Explain
+      try (OutputStream os = he.getResponseBody()) {
+        os.write(response.getBytes());
       }
     }
   }
@@ -85,7 +95,7 @@ public class RestServer {
 
     }
   }
-  
+
   private List<Player> getPlayers() {
     List<Player> players = new ArrayList();
     players.add(new Player(1, "James Rodríguez", "Columbia"));
@@ -95,16 +105,45 @@ public class RestServer {
     players.add(new Player(5, "van Persie", "Holland"));
     return players;
   }
-  
-  
 
   class HandlerFileServer implements HttpHandler {
 
     @Override
     public void handle(HttpExchange he) throws IOException {
+      int responseCode = 500;
+      //Set initial error values if an un expected problem occurs
+      String errorMsg = null;
+      byte[] bytesToSend = "<h1>Internal Error </h1><p>We are sorry. The server encountered an unexpected problem</p>".getBytes();
+      String mime = null;
+      
       String requestedFile = he.getRequestURI().toString();
       String f = requestedFile.substring(requestedFile.lastIndexOf("/") + 1);
-      String extension = f.substring(f.lastIndexOf("."));
+      try {
+        String extension = f.substring(f.lastIndexOf("."));
+        mime = getMime(extension);
+        File file = new File(publicFolder + f);
+        System.out.println(publicFolder + f);
+        bytesToSend = new byte[(int) file.length()];
+        BufferedInputStream bis = new BufferedInputStream(new FileInputStream(file));
+        bis.read(bytesToSend, 0, bytesToSend.length);
+        responseCode = 200;
+      } catch (Exception e) {
+        responseCode = 404;
+        errorMsg = "<h1>404 Not Found</h1>No context found for request";
+      }
+      if (responseCode == 200) {
+        Headers h = he.getResponseHeaders();
+        h.set("Content-Type", mime);
+      } else {
+        bytesToSend = errorMsg.getBytes();
+      }
+      he.sendResponseHeaders(responseCode, bytesToSend.length);
+      try (OutputStream os = he.getResponseBody()) {
+        os.write(bytesToSend, 0, bytesToSend.length);
+      }
+    }
+
+    private String getMime(String extension) {
       String mime = "";
       switch (extension) {
         case ".pdf":
@@ -123,29 +162,7 @@ public class RestServer {
           mime = "application/java-archive";
           break;
       }
-      File file = new File(publicFolder + f);
-      System.out.println(publicFolder + f);
-      byte[] bytesToSend = new byte[(int) file.length()];
-      String errorMsg = null;
-      int responseCode = 200;
-      try {
-        BufferedInputStream bis = new BufferedInputStream(new FileInputStream(file));
-        bis.read(bytesToSend, 0, bytesToSend.length);
-      } catch (IOException ie) {
-        errorMsg = "<h1>404 Not Found</h1>No context found for request";
-      }
-      if (errorMsg == null) {
-        Headers h = he.getResponseHeaders();
-        h.set("Content-Type", mime);
-      } else {
-        responseCode = 404;
-        bytesToSend = errorMsg.getBytes();
-
-      }
-      he.sendResponseHeaders(responseCode, bytesToSend.length);
-      try (OutputStream os = he.getResponseBody()) {
-        os.write(bytesToSend, 0, bytesToSend.length);
-      }
+      return mime;
     }
   }
 }
